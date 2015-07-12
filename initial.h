ad4bfc8997ba083fbd25fd3f1c125f31814441f3
Heark <hearkofsorcerey@gmail.com>
2015-07-12 00:12:14 -0400

    Update initial.h

--- initial.h
+++ initial.h
@@ -4,3 +4,8 @@ start if { bar.f =  X_to_i.new(if file ~!.it) } return y
 if { begin }.start?
   end.file_processing
 !}
+ main if end.file_processing = start.new defined?
+   def new.start()
+   create.mainFile
+end
+Read_main.file.read()::return::true if file.open? return nil
